a337895a1191e086971bf6a818df81b03dfbf281
Alexander Quispe Quino <Alessix.291296@gmail.com>
2019-07-26 18:47:29 -0500

    Updates

---
+++ precache-manifest.008e60a28b3133c087f0a91466d18ce1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60b2b18ce93845dd1f77b02baa61dc80",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "d973aab02b63b1b4a036",
+    "url": "/react-tutorial/static/css/main.fe148ea3.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/react-tutorial/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "d973aab02b63b1b4a036",
+    "url": "/react-tutorial/static/js/main.5eed2a4a.chunk.js"
+  },
+  {
+    "revision": "2a4d04de927634e03e5b",
+    "url": "/react-tutorial/static/js/runtime~main.782a6e4d.js"
+  }
+]);
