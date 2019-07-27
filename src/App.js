98767473e87e86caf5140ee83b04421fd921a5b0
Alexander Quispe Quino <Alessix.291296@gmail.com>
2019-07-26 19:13:59 -0500

    primer commit react, analizando componentes simples y de clase personalizados como tambien estate y props (una tabla con datos para eliminar)

---
+++ src/App.js
@@ -0,0 +1,100 @@
+// import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+// 
+
+
+import React, {Component} from 'react';
+import Table from './table'
+
+
+// class App extends Component{
+//  render(){
+//    return(
+//      <div className="container">
+//        <Table/>
+    
+//      </div>  
+
+//      )
+//  }
+// }
+
+
+class App extends Component {
+    state = {
+        characters: [
+          {
+            name: 'Charlie',
+            job: 'Janitor',
+          },
+          {
+            name: 'Mac',
+            job: 'Bouncer',
+          },
+          {
+            name: 'Dee',
+            job: 'Aspring actress',
+          },
+          {
+            name: 'Dennis',
+            job: 'Bartender',
+          },
+        ]
+    };
+
+    removeCharacter = index => {
+        const { characters } = this.state;
+    
+        this.setState({
+            characters: characters.filter((character, i) => { 
+                return i !== index;
+            })
+        });
+    }
+
+    // handleSubmit = character => {
+    //     this.setState({characters: [...this.state.characters, character]});
+    // }
+
+    render() {
+        const { characters } = this.state;
+        
+        return (
+            <div className="container">
+                <h1>React Tutorial</h1>
+                <p>Add a character with a name and a job to the table.</p>
+                <Table
+                    characterData={characters}
+                    removeCharacter={this.removeCharacter} 
+                />
+
+            </div>
+        );
+    }
+}
+
+export default App;
